acd10383de89ef12dece16f10ea0ff3224638688
Marco Thomas <ma_tho@web.de>
2020-08-22 11:56:29 +0200

    Update linkdots.sh

--- laptop-nord/linkdots.sh
+++ laptop-nord/linkdots.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+VERBOSE=$1
+
 # Make sure the paths are correct
 path=$(pwd)
 
@@ -8,34 +10,22 @@ rm -rf $HOME/scripts
 ln -sf $path/scripts $HOME/scripts
 
 # Home directory
-ln -sf $path/.dir_colors $HOME/.dir_colors
-ln -sf $path/.gitconfig $HOME/.gitconfig
-ln -sf $path/.tmux.conf $HOME/.tmux.conf
-ln -sf $path/.vimrc $HOME/.vimrc
-ln -sf $path/.Xresources $HOME/.Xresources
-ln -sf $path/.zshrc $HOME/.zshrc
+home_files=".dir_colors .gitconfig .tmux.conf .vimrc .Xresources .zshrc"
+for file in $home_files
+do
+	ln -sf $path/$file $HOME/$file
+	if [ "$VERBOSE" = "-v" ]; then
+		echo "Linked from $path/$file to $HOME/$file"
+	fi
+done
 
 # .config directory
-rm -rf $HOME/.config/alacritty
-ln -sf $path/.config/alacritty $HOME/.config/alacritty
-
-rm -rf $HOME/.config/dunst
-ln -sf $path/.config/dunst $HOME/.config/dunst
-
-rm -rf $HOME/.config/i3
-ln -sf $path/.config/i3 $HOME/.config/i3
-
-rm -rf $HOME/.config/nvim
-ln -sf $path/.config/nvim $HOME/.config/nvim
-
-rm -rf $HOME/.config/picom.conf
-ln -sf $path/.config/picom.conf $HOME/.config/picom.conf
-
-rm -rf $HOME/.config/polybar
-ln -sf $path/.config/polybar $HOME/.config/polybar
-
-rm -rf $HOME/.config/ranger
-ln -sf $path/.config/ranger $HOME/.config/ranger
-
-rm -rf $HOME/.config/rofi
-ln -sf $path/.config/rofi $HOME/.config/rofi
+conf_files="alacritty dunst i3 nvim picom.conf polybar ranger rofi"
+for file in $conf_files
+do
+	rm -rf $HOME/.config/$file
+	ln -sf $path/.config/$file $HOME/.config/$file
+	if [ "$VERBOSE" = "-v" ]; then
+		echo "Linked from $path/.config/$file to $HOME/.config/$file"
+	fi
+done
