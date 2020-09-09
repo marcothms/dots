b78fb45584e8327cf0c6efea4a25a822255c3dd7
Marco Thomas <mthomas@marcmk.de>
2020-09-09 22:16:38 +0200

    Some changes

--- laptop-nord/.config/polybar/launch.sh
+++ laptop-nord/.config/polybar/launch.sh
@@ -4,13 +4,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
   for mon in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     if [ "$1" = "laptop" ]; then
-      MONITOR=$mon polybar --reload workspaces &
-#      MONITOR=$mon polybar --reload music &
-      MONITOR=$mon polybar --reload status &
-      MONITOR=$mon polybar --reload tray &
-    fi
-    if [ "$1" = "all" ]; then
-      MONITOR=$mon polybar --reload all &
+      MONITOR=$mon polybar --reload main &
     fi
   done
 else
