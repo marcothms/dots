f2d5836710cdf9665e8cde96dc2ccc21011bb23a
Marco Thomas <mail@marco-thomas.net>
2021-02-09 16:34:28 +0100

    Fixing polybar

--- files/scripts/polybar.sh
+++ files/scripts/polybar.sh
@@ -4,7 +4,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
   for mon in $(xrandr --query | grep " connected" | cut -d" " -f1); do
       MONITOR=$mon polybar --reload workspaces &
-      #MONITOR=$mon polybar --reload music &
+      MONITOR=$mon polybar --reload music &
       MONITOR=$mon polybar --reload tray &
     if [ $mon = "eDP"  ]; then
       MONITOR=$mon polybar --reload info_laptop &
