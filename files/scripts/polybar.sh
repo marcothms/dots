76d8cf19eee29c21deec97ebb96331e156261d15
Marco Thomas <mail@marco-thomas.net>
2021-02-15 16:08:14 +0100

    Update polybar

--- files/scripts/polybar.sh
+++ files/scripts/polybar.sh
@@ -3,14 +3,15 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for mon in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-      MONITOR=$mon polybar --reload workspaces &
-      MONITOR=$mon polybar --reload music &
-      MONITOR=$mon polybar --reload tray &
-    if [ $mon = "eDP"  ]; then
-      MONITOR=$mon polybar --reload info_laptop &
-    else
-      MONITOR=$mon polybar --reload info &
-    fi
+      MONITOR=$mon polybar --reload main &
+#      MONITOR=$mon polybar --reload workspaces &
+#      MONITOR=$mon polybar --reload music &
+#      MONITOR=$mon polybar --reload tray &
+#    if [ $mon = "eDP"  ]; then
+#      MONITOR=$mon polybar --reload info_laptop &
+#    else
+#      MONITOR=$mon polybar --reload info &
+#    fi
   done
 else
     echo "No Bars loaded."
