a02448eaa01ce6aa025e741822a651fe34b004a6
Marco Thomas <mail@marco-thomas.net>
2021-09-15 16:40:34 +0200

    Update wallpaper

--- files/scripts/wallpaper.sh
+++ files/scripts/wallpaper.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-feh --bg-fill $HOME/data/Seafile/images/wallpaper/wallpaper.png
+if [ `hostname` == "nazarick" ]; then
+	feh --bg-fill $HOME/data/Seafile/images/wallpaper/wallpaper-219.png
+else
+	feh --bg-fill $HOME/data/Seafile/images/wallpaper/wallpaper-169.png
+fi
