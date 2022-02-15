cce9902367e9f6a330a76b87de96174b5a3a56f1
Marco Thomas <39596050+marcothms@users.noreply.github.com>
2022-02-16 00:24:32 +0100

    update emacs.el path for tangle

--- files/emacs/.emacs.d/init.el
+++ files/emacs/.emacs.d/init.el
@@ -1,4 +1,4 @@
 (require 'org)
-(find-file (concat (getenv "HOME") "/dots/files/emacs/.emacs.d/init.org"))
+(find-file (concat (getenv "HOME") "/.dots/files/emacs/.emacs.d/init.org"))
 (org-babel-tangle)
-(load-file (concat (getenv "HOME") "/dots/files/emacs/.emacs.d/init.el"))
+(load-file (concat (getenv "HOME") "/.dots/files/emacs/.emacs.d/init.el"))
