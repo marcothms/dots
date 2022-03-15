509f4dbf86b3ae354d31070211edd34fa706a348
Marco Thomas <39596050+marcothms@users.noreply.github.com>
2022-03-15 21:45:19 +0100

    Delete files/emacs/.emacs.d directory

--- files/emacs/.emacs.d/init.el
+++
@@ -1,4 +0,0 @@
-(require 'org)
-(find-file (concat (getenv "HOME") "/.dots/files/emacs/.emacs.d/init.org"))
-(org-babel-tangle)
-(load-file (concat (getenv "HOME") "/.dots/files/emacs/.emacs.d/init.el"))
