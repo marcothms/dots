d9a8de62b12b33f845fa28dd2ee88e4eb1876074
Marco Thomas <rb.ma.tho@gmail.com>
2023-02-08 18:32:54 +0100

    picom: add shadow and adjust corner radius

--- files/i3/.config/i3/i3-windows.sh
+++ files/i3/.config/i3/i3-windows.sh
@@ -7,7 +7,7 @@ for id in $ids
 do names="$names $(xprop -id $id| grep WM_CLASS |tr -d '" '|cut -d "=" -f2|awk -F "," '{print $NF}')($id)"
 done
 
-flags='-m 0 -i -l 10 -p window -fn SFMono-10 -nb #2f383e -nf #d3c6aa -sb #93b259 -sf #2f383e'
+flags='-m 0 -i -l 10 -p window -fn SFMono-11 -nb #2f383e -nf #d3c6aa -sb #93b259 -sf #2f383e'
 
 # Show Open window class names as a dmenu option
 target=$(echo $names | tr " " "\n" | dmenu $flags | grep -Eo "\(.*\)" | tr -d "()" )
