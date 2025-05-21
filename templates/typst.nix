56fd0796cb0650e48a377562da5a56ba3e1bf205
Marco Thomas <github@marcothms.de>
2025-05-21 10:57:57 +0900

    typst: bump version

--- templates/typst.nix
+++ templates/typst.nix
@@ -1,7 +1,12 @@
+# Before updating:
+#
+# 1. Make sure to be on unstable
+# 2. nix-channel --update
+# 
 with (import <nixpkgs> {});
 mkShell {
   buildInputs = [
-    (typst.overrideAttrs { version = "0.12.0"; })
+    (typst.overrideAttrs { version = "0.13.1"; })
     tinymist
   ];
 }
