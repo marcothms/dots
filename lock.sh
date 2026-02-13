ebc61672538195570f8fe3d6135474992ba1a75c
Marco Thomas <git@marcothms.de>
2026-02-13 10:34:52 +0100

    ft: correct ssh-agent reset on lock

--- lock.sh
+++ lock.sh
@@ -1,1 +1,4 @@
-dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock && ssh-add -D
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+
+ssh-add -D
+dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock
